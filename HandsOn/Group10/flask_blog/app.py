984459ff156c30e5777d2f088cfa0a07c00fabf3
Julia-upc <juliasanchezmartinez98@gmail.com>
2021-11-12 20:31:13 +0100

    c

--- HandsOn/Group10/flask_blog/app.py
+++ HandsOn/Group10/flask_blog/app.py
@@ -8,14 +8,15 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/madridEvents')
+@app.route('/madridEvents', )
 
 def index():
 	headers = ['Event Title','Init Date','End Date','Location','Price','Link']
 	table = query_inicial()
-	return render_template('index.html',headers=headers, objects=table)
-        
-    
+	if request.method == 'POST':
+		return "<p>Hello, World!</p>"
+	else:
+		return render_template('index.html',headers=headers, objects=table)
 	
 
 
@@ -26,13 +27,27 @@ def query_inicial():
 	g.namespace_manager.bind('default', Namespace("http://smartcity.linkeddata.es/lcc/ontology/MadridEvents#"),override=False)
 	g.parse(path+'/rdf/RDF-with-links.ttl', format="turtle")
 	default=Namespace("http://smartcity.linkeddata.es/lcc/ontology/MadridEvents#")
-	q = prepareQuery('''SELECT ?title ?startDate ?location ?endDate ?price ?link WHERE {   ?event a default:Event. ?event default:title ?title. ?event default:startDate ?startDate. ?event default:takesPlaceIn ?location. ?event default:endDate ?endDate. ?event default:price ?price. ?event default:URI ?link} LIMIT 10 ''',initNs = { "default": default})
+	q = prepareQuery('''
+	SELECT ?title ?startDate ?endDate ?location  ?price ?link WHERE { 
+	?event a default:Event.
+	?event default:title ?title.
+	?event default:startDate ?startDate.
+	?event default:takesPlaceIn ?location.
+	?event default:endDate ?endDate.
+	?event default:price ?price.
+	?event default:URI ?link
+	}
+	''',
+	initNs = { "default": default} 
+	)
+
 	queryOut = g.query(q)
+	datos=[]
+	
+	for event, startDate, location, endDate, price, link in queryOut:
+		datos.append([str(event), str(startDate), str(endDate), str(location), str(price), str(link)])
 
-	return queryOut
-
-
-
+	return(datos)
 
 
 
