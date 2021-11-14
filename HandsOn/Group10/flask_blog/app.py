e5b1aab7dba989372a47d1689b2b515a38759951
Julia-upc <juliasanchezmartinez98@gmail.com>
2021-11-14 13:49:37 +0100

    changes

--- HandsOn/Group10/flask_blog/app.py
+++ HandsOn/Group10/flask_blog/app.py
@@ -6,8 +6,7 @@ from flask import request
 from rdflib import Graph, Namespace, Literal
 from rdflib.namespace import RDF, RDFS
 from rdflib.plugins.sparql import prepareQuery
-
-
+import datetime
 
 app = Flask(__name__)
 
@@ -22,66 +21,12 @@ def index():
 		req1 = request.form.get("fin")
 		req2 = request.form.get("grat")
 		req3 = request.form.get("local")
-		if req == "" and req1 == "" and str(req2) == "None" and req3 == "Todas":
-			pass
-		elif req != "" and req1 == "" and str(req2) == "None" and req3 == "Todas":
-			#SOLO SE INDICA FECHA INICIAL
-			table = 2
-		elif req == "" and req1 != "" and str(req2) == "None" and req3 == "Todas":
-			#SOLO SE INDICA FECHA FINAL
-			table = 3
-		elif req == "" and req1 == "" and req2 != "None" and req3 == "Todas":
-			#SOLO SE BUSCAN EVENTOS GRATUITOS
-			table = buscarEventosGratuitos()
-			return render_template('index.html',headers=headers, objects=table)
-
-		elif req == "" and req1 == "" and str(req2) == "None" and req3 != "Todas":
-			#SOLO SE BUSCA UN TIPO DE EVENTOS
-			table = buscarTipoDeEvento(req3)
-			return render_template('index.html',headers=headers, objects=table)
-		elif req != "" and req1 != "" and req2 != "None" and req3 == "Todas":
-			#SE INDICAN FECHA INI Y FECHA FIN
-			table = 4
-		elif req != "" and req1 == "" and req2 != "None" and req3 == "Todas":
-			#SE INDICAN FECHA INI Y GRATUITO
-			table = 5
-		elif req != "" and req1 == "" and str(req2) == "None" and req3 != "Todas":
-			#SE INDICAN FECHA INI Y EVENTOS
-			table = 6
-		elif req == "" and req1 != "" and req2 != "None" and req3 == "Todas":
-			#SE INDICAN FECHA FIN Y GRATUITO
-			table = 7
-		elif req == "" and req1 != "" and str(req2) == "None" and req3 != "Todas":
-			#SE INDICAN FECHA FIN Y EVENTOS
-			table = 8
-		elif req == "" and req1 == "" and req2 != "None" and req3 != "Todas":
-			#SE INDICAN GRATUITO Y EVENTOS
-			table = 9
-		elif req != "" and req1 != "" and req2 != "None" and req3 == "Todas":
-			#SE INDICAN FECHA INI FECHA FIN Y  GRATUITO
-			table = 10
-		elif req != "" and req1 != "" and str(req2) == "None" and req3 != "Todas":
-			#SE INDICAN FECHA INI FECHA FIN Y EVENTOS
-			table = 11
-		elif req != "" and req1 == "" and req2 != "None" and req3 != "Todas":
-			#SE INDICAN FECHA INI GRATUITO Y EVENTOS
-			table = 12
-		elif req == "" and req1 != "" and req2 != "None" and req3 != "Todas":
-			#SE INDICAN FECHA FIN GRATUITO Y EVENTOS
-			table = 13
-		elif req != "" and req1 != "" and req2 != "None" and req3 != "Todas":
-			#SE INDICAN FECHA INI FECHA FIN GRATUITO Y EVENTOS
-			table = 14
-
-		return "<p>Init:"+str(req)+ "    Fin: "+str(req1)+"     Gat: "+str(req2)+"      Local: "+str(req3)+ " Table: "+str(table)+"</p>"
-
+		table = queries(gratuito=req2, fecha_in=req, fecha_ax=req1, tipo=req3)
+		return render_template('index.html',headers=headers, objects=table)
 	else:
 		table = query_inicial()
 		return render_template('index.html',headers=headers, objects=table)
-
-
-
-
+		
 def query_inicial():
 	RDFsource = "https://raw.githubusercontent.com/pablo-crucera/Curso2021-2022-ODKG/master/HandsOn/eventosfinal-deverdad.ttl"
 	g = Graph()
@@ -113,7 +58,8 @@ def query_inicial():
 
 
 
-def buscarEventosGratuitos():
+
+def queries(gratuito=None, fecha_in=None, fecha_ax=None, tipo=None):
 	RDFsource = "https://raw.githubusercontent.com/pablo-crucera/Curso2021-2022-ODKG/master/HandsOn/eventosfinal-deverdad.ttl"
 	g = Graph()
 	g.namespace_manager.bind('ns', Namespace("http://www.example.org#"), override=False)
@@ -123,56 +69,54 @@ def buscarEventosGratuitos():
 	g.parse(RDFsource, format="ttl")
 	smart = Namespace("http://smartcity.linkeddata.es/lcc/ontology/MadridEvents#")
 	xsd = Namespace("http://www.w3.org/2001/XMLSchema#")
-	q = prepareQuery('''
-		SELECT ?title ?startDate ?location ?endDate ?price ?link WHERE {
-        ?event a smart:Event.
-        ?event smart:title ?title.
-        ?event smart:startDate ?startDate.
-        ?event smart:takesPlaceIn ?facility.
-        ?facility smart:name ?location.
-        ?event smart:endDate ?endDate.
-        ?event smart:price ?price.
-        ?event smart:URI ?link.
-        ?event smart:free "true"^^xsd:boolean
-      	}
-    	''',
-		initNs = { "smart": smart, "xsd": xsd}
-	)
+
+	if fecha_in == "":
+		fecha_min = "2020-01-01T00:00:00Z"
+	else:
+		day_month_year = fecha_in.split('-')
+		fecha_min = str(datetime.datetime(int(day_month_year[0]), int(day_month_year[1]), int(day_month_year[2]))).replace(" ","T") + "Z"
+			
+	if fecha_ax == "":
+		fecha_max = "2023-01-01T00:00:00Z"
+	else:
+		day_month_year = fecha_ax.split('-')
+		fecha_max = str(datetime.datetime(int(day_month_year[0]), int(day_month_year[1]), int(day_month_year[2]))).replace(" ","T") + "Z"
+
+	if gratuito == "gratis":
+		grat = True
+	else:
+		grat = False
+	
+	if tipo == "Todas":
+		buscaPorTipo = False
+	else:
+		buscaPorTipo = True
+
+	q = '''
+	SELECT ?title ?startDate ?endDate ?location ?price ?link WHERE {
+		?event a smart:Event.
+		?event smart:title ?title.
+		?event smart:startDate ?startDate.
+		?event smart:takesPlaceIn ?facility.
+		?facility smart:name ?location.
+		?event smart:endDate ?endDate.
+		?event smart:price ?price.
+		?event smart:URI ?link.
+		FILTER(?startDate > "''' + fecha_min + '''"^^xsd:dateTime && ?endDate < "''' + fecha_max + '''"^^xsd:dateTime)
+	    '''
+	
+	if grat:
+		q = q + '''?event smart:free "true"^^xsd:boolean.
+			'''
+
+	if buscaPorTipo:
+		q = q + '''?event smart:isA ?category.
+			?category smart:name "''' + tipo + '''".
+			'''
+	
+	q = q + '''}'''
+	
+	q = prepareQuery(q, initNs = { "smart": smart, "xsd": xsd})
 
 	queryOut = g.query(q)
 	return queryOut
-
-
-###### QUERY TIPO DE EVENTO #######
-
-def buscarTipoDeEvento(nombre_tipo):
-	# Introducir nombre del tipo en formato string
-	RDFsource = "https://raw.githubusercontent.com/pablo-crucera/Curso2021-2022-ODKG/master/HandsOn/eventosfinal-deverdad.ttl"
-	g = Graph()
-	g.namespace_manager.bind('ns', Namespace("http://www.example.org#"), override=False)
-	g.namespace_manager.bind('vcard', Namespace("http://www.w3.org/2001/vcard-rdf/3.0#"), override=False)
-	g.namespace_manager.bind('xsd', Namespace("http://www.w3.org/2001/XMLSchema#"), override=False)
-	g.namespace_manager.bind('smart', Namespace("http://smartcity.linkeddata.es/lcc/ontology/MadridEvents#"), override=False)
-	g.parse(RDFsource, format="ttl")
-	smart = Namespace("http://smartcity.linkeddata.es/lcc/ontology/MadridEvents#")
-	xsd = Namespace("http://www.w3.org/2001/XMLSchema#")
-	q = prepareQuery('''
-		SELECT ?title ?startDate ?endDate ?location ?price ?link WHERE {
-		 ?event a smart:Event.
-		 ?event smart:title ?title.
-		 ?event smart:startDate ?startDate.
-		 ?event smart:takesPlaceIn ?facility.
-		 ?facility smart:name ?location.
-		 ?event smart:endDate ?endDate.
-		 ?event smart:price ?price.
-		 ?event smart:URI ?link.
-		 ?event smart:isA ?category.
-		 ?category smart:name "'''+ nombre_tipo + '''"
-		}
-		''',
-		initNs = { "smart": smart, "xsd": xsd}
-	)
-
-	queryOut = g.query(q)
-
-	return(g.query(q))
